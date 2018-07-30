3ea79b1e780c6d396c8ba7ee5a558e2347f98d75
adilahmad094 <adilahmad094@gmail.com>
2018-07-30 12:06:21 +0530

    one way Tab completed - 100% working

--- app/src/main/java/com/example/ashish/justgetit/one_way_layout.java
+++ app/src/main/java/com/example/ashish/justgetit/one_way_layout.java
@@ -1,4 +1,32 @@
 package com.example.ashish.justgetit;
 
-public class one_way_layout {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ashish.justgetit.R;
+import com.example.ashish.justgetit.oneway_finalbooking;
+
+public class one_way_layout extends AppCompatActivity {
+
+    private Button oneWayNextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_way_layout);
+
+        oneWayNextButton = findViewById(R.id.oneWayNextButton);
+
+        oneWayNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(one_way_layout.this, oneway_finalbooking.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
