05cf38386536cc17eef8c155d8d7fbc543e75a23
adilahmad094 <adilahmad094@gmail.com>
2018-08-02 21:56:41 +0530

    Your Trips (Completed Trips) UI created

---
+++ app/src/main/java/com/example/ashish/justgetit/navigation_drawer/completed_rides.java
@@ -0,0 +1,16 @@
+package com.example.ashish.justgetit.navigation_drawer;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ashish.justgetit.R;
+
+public class completed_rides extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.completed_rides);
+    }
+}
