f2fd94a23e72928e2f1762c6d487969354ad3f67
adilahmad094 <adilahmad094@gmail.com>
2018-08-09 19:50:17 +0530

    settings UI added

--- app/src/main/java/com/example/ashish/justgetit/navigation_drawer/settings.java
+++ app/src/main/java/com/example/ashish/justgetit/navigation_drawer/settings.java
@@ -2,14 +2,25 @@ package com.example.ashish.justgetit.navigation_drawer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.ashish.justgetit.R;
 
 public class settings extends AppCompatActivity {
 
+    EditText settingsName, settingsPhone, settingsEmail, settingsPassword;
+    ImageView settingsImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        settingsName = findViewById(R.id.settingsName);
+        settingsEmail = findViewById(R.id.settingsEmail);
+        settingsPhone = findViewById(R.id.settingsPhone);
+        settingsPassword = findViewById(R.id.settingsPassword);
+        settingsImageView = findViewById(R.id.settingsImageView);
     }
 }
