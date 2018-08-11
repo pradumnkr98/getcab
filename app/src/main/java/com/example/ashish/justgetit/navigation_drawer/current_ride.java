200e76fc06252f71b3f873a5bd36899a198f22b9
adilahmad094 <adilahmad094@gmail.com>
2018-08-11 12:42:16 +0530

    changes made, drop location removed from roundway, settings UI added with edit account, wallet UI added, completed rides back end done (still little bugged)

--- app/src/main/java/com/example/ashish/justgetit/navigation_drawer/current_ride.java
+++ app/src/main/java/com/example/ashish/justgetit/navigation_drawer/current_ride.java
@@ -1,15 +1,19 @@
 package com.example.ashish.justgetit.navigation_drawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.ashish.justgetit.R;
+import com.example.ashish.justgetit.local_booking.getting_nearby_driver;
 
 public class current_ride extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.current_ride);
+
+        Intent intent = new Intent(current_ride.this, getting_nearby_driver.class);
+        startActivity(intent);
     }
 }
