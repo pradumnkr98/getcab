6d1be40e90bf2550810026d626476864df9a8b09
adilahmad094 <adilahmad094@gmail.com>
2018-08-12 02:34:41 +0530

    Future Ride and Completed Ride UI updated

--- app/src/main/java/com/example/ashish/justgetit/navigation_drawer/Edit_Account.java
+++ app/src/main/java/com/example/ashish/justgetit/navigation_drawer/Edit_Account.java
@@ -1,7 +1,10 @@
 package com.example.ashish.justgetit.navigation_drawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ashish.justgetit.R;
 
@@ -11,5 +14,14 @@ public class Edit_Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__account);
+
+        Button save = (Button)findViewById(R.id.saveEditAccount);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Edit_Account.this,settings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
