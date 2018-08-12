f42a42318a21d47289ef5444e33f4daf70542fb7
adilahmad094 <adilahmad094@gmail.com>
2018-08-12 17:46:41 +0530

    roundway files corrected, back button added in settings toolbar, minor UI tweaks

--- app/src/main/java/com/example/ashish/justgetit/navigation_drawer/settings.java
+++ app/src/main/java/com/example/ashish/justgetit/navigation_drawer/settings.java
@@ -1,25 +1,50 @@
 package com.example.ashish.justgetit.navigation_drawer;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.ashish.justgetit.R;
+import com.example.ashish.justgetit.homepage1;
 import com.example.ashish.justgetit.login_page;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class settings extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        // Back Button in toolbar
+        toolbar = findViewById(R.id.settingsToolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.back_icon);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(settings.this, homepage1.class);
+                startActivity(intent);
+            }
+        });
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
 
         RelativeLayout relativeLayout = findViewById(R.id.accouontSettingsLayout);
         relativeLayout.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/ashish/justgetit/outstation/roundway_gettingnearbydriver.java
+++ app/src/main/java/com/example/ashish/justgetit/outstation/roundway_gettingnearbydriver.java
@@ -1,15 +1,359 @@
 package com.example.ashish.justgetit.outstation;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
 import com.example.ashish.justgetit.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class roundway_gettingnearbydriver extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class roundway_gettingnearbydriver extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener, RoutingListener {
+    GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+    Button request_cab;
+    ProgressDialog progressDialog;
+    private static final int[] COLORS = new int[]{R.color.colorPrimaryDark};
+    LatLng driverlocation, pickuplocation;
+    String pickup_location;
+    double latitude, longitude;
+    private List<Polyline> polylines;
+
+
+    private int radius = 1;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        buildGoogleApiClient();
+        // mMap.setMyLocationEnabled(true);
+        mMap.addMarker(new MarkerOptions().position(driverlocation)).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.caricon));
+
+        pickuplocation = getLocationFromAddress(roundway_gettingnearbydriver.this, pickup_location);
+        mMap.addMarker(new MarkerOptions().position(pickuplocation)).setTitle("Pickup Here..");
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pickuplocation));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+
+        Routing routing = new Routing.Builder()
+                .travelMode(AbstractRouting.TravelMode.DRIVING)
+                .withListener(this)
+                .alternativeRoutes(false)
+                .waypoints(pickuplocation, driverlocation)
+                .build();
+        routing.execute();
+
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+        com.google.android.gms.maps.model.LatLng latLng = new com.google.android.gms.maps.model.LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        Log.e("location", latLng.toString());
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roundway_gettingnearbydriver);
+        // progressDialog = new ProgressDialog(getting_nearby_driver.this);
+        //progressDialog.show();
+
+
+        polylines = new ArrayList<>();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.customer_map);
+        mapFragment.getMapAsync(this);
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            latitude = extras.getDouble("driverlat");
+            longitude = extras.getDouble("driverlong");
+
+            driverlocation = new LatLng(latitude, longitude);
+        }
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        pickup_location = preferences.getString("pickup", "NULL");
+
+
+    }
+
+    // private void getclosestdriver() {
+    //    DatabaseReference driverlocation = FirebaseDatabase.getInstance().getReference().child("Driver Available");
+    //   GeoFire geoFire = new GeoFire(driverlocation);
+    //   GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(pickuplocation.longitude, pickuplocation.latitude), radius);
+       /* geoQuery.removeAllListeners();
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                if (!driverfound) {
+                    driverfound = true;
+                    driverfoundid = key;
+
+                    DatabaseReference driverref = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(driverfoundid);
+                    String customerid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    HashMap map = new HashMap();
+                    map.put("Customer Id", customerid);
+                    driverref.updateChildren(map);
+
+                    getdriverlocation();
+                    request_cab.setText("Looking for driver location....");
+                }
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                if (!driverfound) {
+                    radius++;
+                    getclosestdriver();
+                }
+
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+            }
+        });*/
+    // }
+
+ /*   private void getdriverlocation() {
+
+        DatabaseReference driverlocationref = FirebaseDatabase.getInstance().getReference().child("drivers working").child(driverfoundid).child("l");
+        driverlocationref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    List<Object> map = (List<Object>) dataSnapshot.getValue();
+                    double locationlat = 0;
+                    double locationlan = 0;
+                    request_cab.setText("driver found");
+                    if (map.get(0) != null) {
+                        locationlat = Double.parseDouble(map.get(0).toString());
+
+                    }
+
+                    if (map.get(1) != null) {
+                        locationlan = Double.parseDouble(map.get(1).toString());
+
+                    }
+                    LatLng driverlatlng = new LatLng(locationlat, locationlan);
+                    if (mdrivermarker != null) {
+                        mdrivermarker.remove();
+                    }
+                    Location loc1 = new Location("");
+                    loc1.setLatitude(pickuplocation.latitude);
+                    loc1.setLongitude(pickuplocation.longitude);
+
+                    Location loc2 = new Location("");
+                    loc2.setLatitude(driverlatlng.latitude);
+                    loc2.setLongitude(driverlatlng.longitude);
+
+
+                    float distance = loc1.distanceTo(loc2);
+                    request_cab.setText("Driver found: " + String.valueOf(distance));
+
+                    mdrivermarker = mMap.addMarker(new MarkerOptions().position(driverlatlng).title("Your Driver"));
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }*/
+
+
+    //Converting string address to latlng
+
+    public LatLng getLocationFromAddress(Context context, String inputtedAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng resLatLng = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(inputtedAddress, 5);
+            if (address == null) {
+                return null;
+            }
+
+            if (address.size() == 0) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            resLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        return resLatLng;
+    }
+
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        if (e != null) {
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> arrayList, int shortestRouteIndex) {
+        if (polylines.size() > 0) {
+            for (Polyline poly : polylines) {
+                poly.remove();
+            }
+        }
+
+        polylines = new ArrayList<>();
+        //add route(s) to the map.
+        for (int i = 0; i < arrayList.size(); i++) {
+
+            //In case of more than 5 alternative routes
+            int colorIndex = i % COLORS.length;
+
+            PolylineOptions polyOptions = new PolylineOptions();
+            polyOptions.color(getResources().getColor(COLORS[colorIndex]));
+            polyOptions.width(10 + i * 3);
+            polyOptions.addAll(arrayList.get(i).getPoints());
+            Polyline polyline = mMap.addPolyline(polyOptions);
+            polylines.add(polyline);
+
+            Toast.makeText(getApplicationContext(), "Route " + (i + 1) + ": distance - " + arrayList.get(i).getDistanceValue() + ": duration - " + arrayList.get(i).getDurationValue(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+
+    public void erasepolylines() {
+        for (Polyline line : polylines) {
+            line.remove();
+        }
+        polylines.clear();
     }
 }
+
